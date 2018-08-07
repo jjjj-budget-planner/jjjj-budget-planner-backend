57f797e6a2689d7040f0783a4daaf338ec1ceb08
Jed Arndt <jedarndt@hotmail.com>
2018-08-07 20:24:03 +0100

    adding cors annotation

--- src/main/com/neo4j/budgetPlanner/HeathCheckController.java
+++ src/main/com/neo4j/budgetPlanner/HeathCheckController.java
@@ -1,11 +1,13 @@
 package com.neo4j.budgetPlanner;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HeathCheckController {
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/health")
         public String index() {
             return "Budget Planner Application is running!!";
